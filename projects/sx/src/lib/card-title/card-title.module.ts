8f080d32299d57d63a0e2e479b3e8f626761411a
Thiago Bloomfield <thiago.bloomfield@salux.com.br>
2025-02-13 14:40:57 -0300

    adiciona card-title

---
+++ projects/sx/src/lib/card-title/card-title.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CardTitleComponent } from "./card-title.component";
+
+@NgModule({
+  declarations: [CardTitleComponent],
+  imports: [CommonModule],
+  exports: [CardTitleComponent],
+})
+export class SxCardTitleModule {}
