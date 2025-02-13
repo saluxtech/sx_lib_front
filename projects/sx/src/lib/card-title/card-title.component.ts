f43b4583edf74472f14fe30db20cc5c999164750
Thiago Bloomfield <thiago.bloomfield@salux.com.br>
2025-02-13 14:40:57 -0300

    adiciona card-title

---
+++ projects/sx/src/lib/card-title/card-title.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sx-card-title',
+  templateUrl: './card-title.component.html',
+  styleUrls: ['./card-title.component.scss']
+})
+export class CardTitleComponent {
+
+}
