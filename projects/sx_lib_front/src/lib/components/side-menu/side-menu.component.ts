c27afc67ed109cbdefef73aa4620ad1dcb3cbd98
narcisodev <narcisoalmeida25@gmail.com>
2025-03-21 10:45:30 -0300

    refactor(side-menu): ajusta tipagem de menuItems do side-menu

--- projects/sx_lib_front/src/lib/components/side-menu/side-menu.component.ts
+++ projects/sx_lib_front/src/lib/components/side-menu/side-menu.component.ts
@@ -8,6 +8,8 @@ import {
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GridSizesPx } from './../../models/grid.model';
+import { SideMenuItem } from './../../models/side-menu.model';
+
 
 @Component({
   selector: 'sx-side-menu',
@@ -17,7 +19,7 @@ import { GridSizesPx } from './../../models/grid.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideMenuComponent implements OnInit {
-  @Input({ required: true }) menuItems: any[] = [];
+  @Input({ required: true }) menuItems: SideMenuItem[] | null = [];
   protected isMenuClosed = signal(false);
 
   @HostListener('window:resize', ['$event'])
