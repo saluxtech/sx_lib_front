47ef7dadd0dc7c06d08b9d97068aff057f5a8265
Guilherme Garcia <guilhermegfa11@gmail.com>
2025-03-27 15:32:19 -0300

    refactor(HIS-1): refatora método de positions do snackbar

--- projects/sx_lib_front/src/lib/models/snackbar/snackbar-position.model.ts
+++ projects/sx_lib_front/src/lib/models/snackbar/snackbar-position.model.ts
@@ -1,4 +1,18 @@
+import { SnackbarPosition } from "../../enums/snackbar-position.enum";
+
 export interface SnackbarPositionInterface {
     horizontalPosition: 'start' | 'center' | 'end';
     verticalPosition: 'top' | 'bottom';
-} 
+}
+
+export const SnackbarPositions: Record<SnackbarPosition, SnackbarPositionInterface> = {
+    [SnackbarPosition.TOP_LEFT]: { horizontalPosition: 'start' as const, verticalPosition: 'top' as const },
+    [SnackbarPosition.TOP_CENTER]: { horizontalPosition: 'center' as const, verticalPosition: 'top' as const },
+    [SnackbarPosition.TOP_RIGHT]: { horizontalPosition: 'end' as const, verticalPosition: 'top' as const },
+    [SnackbarPosition.BOTTOM_LEFT]: { horizontalPosition: 'start' as const, verticalPosition: 'bottom' as const },
+    [SnackbarPosition.BOTTOM_CENTER]: { horizontalPosition: 'center' as const, verticalPosition: 'bottom' as const },
+    [SnackbarPosition.BOTTOM_RIGHT]: { horizontalPosition: 'end' as const, verticalPosition: 'bottom' as const },
+    [SnackbarPosition.CENTER_LEFT]: { horizontalPosition: 'start' as const, verticalPosition: 'bottom' as const },
+    [SnackbarPosition.CENTER]: { horizontalPosition: 'center' as const, verticalPosition: 'bottom' as const },
+    [SnackbarPosition.CENTER_RIGHT]: { horizontalPosition: 'end' as const, verticalPosition: 'bottom' as const }
+};
