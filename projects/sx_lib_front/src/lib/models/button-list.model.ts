dccaec18824e7b7b58b6297a7e64c3d42c7e4ff8
Guilherme Garcia <guilherme.garcia@salux.com.br>
2025-04-04 13:03:56 -0300

    feat(HIS-4): adiciona header-buttons

--- projects/sx_lib_front/src/lib/models/button-list.model.ts
+++ projects/sx_lib_front/src/lib/models/button-list.model.ts
@@ -3,4 +3,6 @@ import { HeaderButtons } from "sx_lib_front";
 export interface ButtonList {
     icon?: string;
     event?: HeaderButtons;
+    variation?: 'raised' | 'stroked' | 'ghost';
+    label?: string;
 }
