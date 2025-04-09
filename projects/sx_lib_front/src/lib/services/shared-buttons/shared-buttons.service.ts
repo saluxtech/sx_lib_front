e2c5b279988f6c141d9bff5c33b4590e30e80d9e
Guilherme Garcia <guilhermegfa11@gmail.com>
2025-04-09 08:31:38 -0300

    fix(HIS-1): muda arquivo de exportação

--- projects/sx_lib_front/src/lib/services/shared-buttons/shared-buttons.service.ts
+++ projects/sx_lib_front/src/lib/services/shared-buttons/shared-buttons.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HeaderButton } from '../../models/header-button.model';
+import { HeaderButtonEvent } from 'sx_lib_front';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderButtonsService {
-  private subject = new Subject<HeaderButton | null>();
+  private subject = new Subject<HeaderButtonEvent | null>();
   public headerButtons$ = this.subject.asObservable();
 
   constructor() { }
 
-  emit(button: HeaderButton) {
+  emit(button: HeaderButtonEvent) {
     this.subject.next(button);
   }
 
