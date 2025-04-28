2d71526e4f52d66f282872d7c781dfb54f23c0a2
Narciso Gomes <narciso.gomes@salux.com.br>
2025-04-28 14:17:51 -0300

    feat(HIS-671): ajusta parametros do basic-card para personalizacao conforme o figma

--- projects/sx_lib_front/src/lib/components/basic-card/basic-card.types.ts
+++ projects/sx_lib_front/src/lib/components/basic-card/basic-card.types.ts
@@ -1,4 +1,4 @@
 export type BasicCardColorVariation = 'white' | 'gray' | 'blue' | 'blue-notification';
-export type BasicCardPadding = '0' | 'sm' | 'md' | 'lg';
+export type BasicCardPadding = '0' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type BasicCardMargingBottom = '0' | 'sm' | 'md' | 'lg' | 'default';
-export type BasicCardRadius = 'sm' | 'md';
+export type BasicCardRadius = 'xs' | 'sm' | 'sm-2' | 'md';
