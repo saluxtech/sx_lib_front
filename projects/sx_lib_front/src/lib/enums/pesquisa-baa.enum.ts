d095fbb9e0adab0328f0fb7687bd750420cfb983
Guilherme Garcia <guilherme.garcia@salux.com.br>
2025-04-29 08:52:53 -0300

    feat(HIS-819): adiciona modal de baa movimentavel

--- projects/sx_lib_front/src/lib/enums/pesquisa-baa.enum.ts
+++ projects/sx_lib_front/src/lib/enums/pesquisa-baa.enum.ts
@@ -1,9 +1,9 @@
 export enum PesquisaBaaEnum {
-    BAA_EM_ABERTO = 'A',
-    BAA_MOVIMENTAVEL = 'M',
+    EM_ABERTO = 'A',
+    MOVIMENTAVEL = 'M',
     TODOS_OS_PACIENTES = 'T',
     APENAS_NO_INICIO = 'I',
     EM_QUALQUER_PARTE = 'Q',
     NOME_DO_PACIENTE = 'NP',
-    NUMERO_DE_BAA = 'NBAA'
+    NUMERO = 'NBAA'
 }
