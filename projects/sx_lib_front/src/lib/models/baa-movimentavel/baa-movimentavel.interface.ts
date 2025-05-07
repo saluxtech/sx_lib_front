feb08eddf0c022d45d8498bdad2b7a770ada713b
Guilherme Garcia <guilherme.garcia@salux.com.br>
2025-05-07 09:13:32 -0300

    modais-mockados: adiciona interface de baa-movimentavel

---
+++ projects/sx_lib_front/src/lib/models/baa-movimentavel/baa-movimentavel.interface.ts
@@ -0,0 +1,8 @@
+export interface BaaMovimentavel {
+    pl_hosp_logado: number,
+    ps_tipo_select: string,
+    ps_texto: string,
+    ps_tipo_pesq: string,   
+    pl_ano_baa: string,
+    pl_nr_baa: string,
+}
